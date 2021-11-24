27756bddbef625fe2c9c93499c6e37e6bc75acd0
Andrew <umanking@gmail.com>
2021-11-24 16:28:02 +0900

    Initial commit

---
+++ src/test/java/com/example/refactoringexample/RefactoringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refactoringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
